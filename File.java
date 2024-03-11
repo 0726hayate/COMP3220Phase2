ca14249e7aa08a57f52c6c63ca442bec2f1c7d55
Farzana Moury <farzm.business@gmail.com>
2024-03-11 12:39:35 -0400

    update file class

--- File.java
+++ File.java
@@ -1,36 +1,34 @@
+import java.util.*;
+
 /**
  * File class represents an individual file that contains information regarding a given dataset
  *
  * @author Farzana Moury
- * @version 1.0
+ * @version 1.1
  * @since Mar-11-24
  */
 public class File {
     private String fileName;
     private int fileSize = 0;
-    private String fileContent;
-    private Field field;
+    private Map<String, List<Object>> content;
 
     /**
      * default constructor
      */
     public File(){
         this.fileName = "sample-file.txt";
-        this.fileContent = "";
-        this.field = null; // no field assigned yet
+        this.content = null;
     }
 
     /**
      * overloaded constructor: add parameters yourself
      * @param fileName the name of the file
-     * @param fileContent the content of the file in string
-     * @param field the field associated with the file
+     * @param content the content of the file as a list of fields
      */
-    public File(String fileName, String fileContent, Field field){
+    public File(String fileName, Map<String, List<Object>> content){
         this.fileName = fileName;
-        this.fileContent = fileContent;
-        this.field = field;
-        this.fileSize = fileContent.length();
+        this.content = content;
+        this.fileSize = content.size();
     }
 
     /**
@@ -66,34 +64,18 @@ public class File {
     }
 
     /**
-     * getter for fileContent
-     * @return fileContent
+     * getter for file content
+     * @return content
      */
-    public String getFileContent() {
-        return fileContent;
+    public Map<String, List<Object>> getContent() {
+        return content;
     }
 
     /**
-     * setter for fileContent
-     * @param fileContent the content of the file
+     * setter for file content
+     * @param content the content is a list of fields
      */
-    public void setFileContent(String fileContent) {
-        this.fileContent = fileContent;
-    }
-
-    /**
-     * getter for field
-     * @return field
-     */
-    public Field getField() {
-        return field;
-    }
-
-    /**
-     * setter for field
-     * @param field the field associated with the file
-     */
-    public void setField(Field field) {
-        this.field = field;
+    public void setContent(Map<String, List<Object>> content) {
+        this.content = content;
     }
 }
