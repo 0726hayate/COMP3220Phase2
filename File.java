e4237350837f2165027f186fc561d6c1426ae8e0
Farzana Moury <farzm.business@gmail.com>
2024-03-11 14:02:47 -0400

    update file class

--- File.java
+++ File.java
@@ -4,13 +4,14 @@ import java.util.*;
  * File class represents an individual file that contains information regarding a given dataset
  *
  * @author Farzana Moury
- * @version 1.1
+ * @version 1.2
  * @since Mar-11-24
  */
 public class File {
     private String fileName;
     private int fileSize = 0;
-    private Map<String, List<Object>> content;
+    private Field content;
+    private String filePath;
 
     /**
      * default constructor
@@ -25,10 +26,10 @@ public class File {
      * @param fileName the name of the file
      * @param content the content of the file as a list of fields
      */
-    public File(String fileName, Map<String, List<Object>> content){
+    public File(String fileName, Field content){
         this.fileName = fileName;
         this.content = content;
-        this.fileSize = content.size();
+        this.fileSize = content.getFieldNames().size();
     }
 
     /**
@@ -67,7 +68,7 @@ public class File {
      * getter for file content
      * @return content
      */
-    public Map<String, List<Object>> getContent() {
+    public Field getContent() {
         return content;
     }
 
@@ -75,7 +76,39 @@ public class File {
      * setter for file content
      * @param content the content is a list of fields
      */
-    public void setContent(Map<String, List<Object>> content) {
-        this.content = content;
+    public void setContent(Field content) { this.content = content; }
+
+
+    public static void main(String[] args) {
+        Field content = new Field();
+
+        // adding the FID field with values
+        List<Object> fid_values = Arrays.asList(0, 1, 2, 3);
+        List<Object> list = new ArrayList<>(fid_values);
+        content.addField("FID", list);
+
+        // adding the arena field with values
+        List<Object> arena_values = Arrays.asList("Capri Pizzeria Recreation Complex", "Adie Knox Arena", "Forest Glade Arena", "WFCU Centre");
+        list = new ArrayList<>(arena_values);
+        content.addField("ARENA", list);
+
+        // adding the address field with values
+        List<Object> address_values = Arrays.asList("2555 Pulford St", "1551 Wyandotte St W" , "3205 Forest Glade Dr", "8787 McHugh St");
+        list = new ArrayList<>(address_values);
+        content.addField("ADDRESS", list);
+
+        // adding the x field with values
+        List<Object> x_values = Arrays.asList(-83.035508601, -83.0531282837 , -82.9150971889, -82.9276320926);
+        list = new ArrayList<>(x_values);
+        content.addField("X", list);
+
+        // adding the y field with values
+        List<Object> y_values = Arrays.asList(42.2579203855, 42.3068279246, 42.3042142342, 42.3187335808);
+        list = new ArrayList<>(y_values);
+        content.addField("Y", list);
+
+        File file = new File("Arenas", content);
+        System.out.println(file.getContent().getFieldNames());
+        System.out.println(file.getContent().getValues("FID"));
     }
 }
