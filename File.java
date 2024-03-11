199af11590d8d14d2673df4dbcaecd295d8c85ff
Farzana Moury <farzm.business@gmail.com>
2024-03-11 17:07:34 -0400

    add file class

--- File.java
+++ File.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 /**
  * File class represents an individual file that contains information regarding a given dataset
  *
@@ -105,55 +103,4 @@ public class File {
     public int numberOfRows(){
         return content.getValuesCountPerField();
     }
-
-
-    public static void main(String[] args) {
-        Field content = new Field();
-
-        // adding the FID field with values
-        List<Object> fid_values = Arrays.asList(0, 1, 2, 3);
-        List<Object> list = new ArrayList<>(fid_values);
-        content.addField("FID", list);
-
-        // adding the arena field with values
-        List<Object> arena_values = Arrays.asList("Capri Pizzeria Recreation Complex", "Adie Knox Arena", "Forest Glade Arena", "WFCU Centre");
-        list = new ArrayList<>(arena_values);
-        content.addField("ARENA", list);
-
-        // adding the address field with values
-        List<Object> address_values = Arrays.asList("2555 Pulford St", "1551 Wyandotte St W" , "3205 Forest Glade Dr", "8787 McHugh St");
-        list = new ArrayList<>(address_values);
-        content.addField("ADDRESS", list);
-
-        // adding the x field with values
-        List<Object> x_values = Arrays.asList(-83.035508601, -83.0531282837 , -82.9150971889, -82.9276320926);
-        list = new ArrayList<>(x_values);
-        content.addField("X", list);
-
-        // adding the y field with values
-        List<Object> y_values = Arrays.asList(42.2579203855, 42.3068279246, 42.3042142342, 42.3187335808);
-        list = new ArrayList<>(y_values);
-        content.addField("Y", list);
-
-        File file = new File("Arenas", content);
-        System.out.println(file.getContent().getFieldNames());
-
-        //get the number of columns and rows
-        System.out.println(file.numberOfColumns());
-        System.out.println(file.numberOfRows());
-
-        // printing the contents of the file
-        for(int i=0; i<file.numberOfColumns(); i++){
-            System.out.format("%-25s",content.getColumns().get(i));
-        }
-
-        System.out.println();
-
-        for(int i=0; i<file.numberOfRows(); i++){
-            System.out.format("%-24s %-24s %-24s %-24s %-24s\n", content.getValues("FID").get(i), content.getValues("ADDRESS").get(i),
-                    content.getValues("X").get(i), content.getValues("Y").get(i), content.getValues("ARENA").get(i));
-        }
-
-    }
-
 }
