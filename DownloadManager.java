d35506390ebb887cd2cc94a4b5314cd8097b577e
sammanku <148726011+sammanku@users.noreply.github.com>
2024-03-13 14:04:44 -0400

    Update DownloadManager.java
    Removed errors from previous update

--- DownloadManager.java
+++ DownloadManager.java
@@ -3,11 +3,6 @@ import java.io.IOException;
 
 /**
  * Download manager that is responsible for downloading the requested file in the specified available file formats
- * @author Samarveer Mankuimport java.io.FileWriter;
-import java.io.IOException;
-
-/**
- * Download manager that is responsible for downloading the requested file in the specified available file formats
  * @author Samarveer Manku
  * @version 1.1
  * @since March 11 2024
@@ -18,7 +13,6 @@ public class DownloadManager {
 
     /**
      * Specify file to download
-     *
      * @param fileName file that user wants to download
      */
     public void selectToDownload(String fileName) {
@@ -27,7 +21,6 @@ public class DownloadManager {
 
     /**
      * Get the file format from user and download it as that file
-     *
      * @param fileType File extension the user requested
      * @param files    File manager with all available files used that will be used to get file content to download
      * @throws IOException FileWriter needs to throw an I/O exception if it fails write into the specified file
@@ -58,64 +51,6 @@ public class DownloadManager {
             for (int j = 0; j < file.numberOfColumns(); j++) {
                 fileContent += String.format("%-24s ", content.getValues(columns[j]).get(i));
                 if (fileType.equals(".csv")) {
-                    fileContent += ",";
-                }
-            }
-            fileContent += "\n";
-        }
-
-        w.write(fileContent);
-        w.close();
-    }
-}
-
- * @version 1.1
- * @since March 11 2024
- */
-
-public class DownloadManager{
-    private String fileToDownload;
-
-    /**
-     * Specify file to download
-     * @param fileName file that user wants to download
-     */
-    public void selectToDownload(String fileName){
-        fileToDownload = fileName;//create file
-    }
-    /**
-     * Get the file format from user and download it as that file
-     * @param fileType File extension the user requested
-     * @param files File manager with all available files used that will be used to get file content to download
-     * @throws IOException FileWriter needs to throw an I/O exception if it fails write into the specified file
-     */
-    public void downloadAs(String fileType, FileManager files) throws IOException{
-        String name = files.getFile().getFileName();
-        File file = files.getFile();
-        Field content = file.getContent();
-        int index = name.lastIndexOf(".");
-        name = name.substring(0, index+1);
-        name = name + fileType;
-
-        FileWriter w = new FileWriter(name);
-        String columns[] = new String[file.numberOfColumns()];
-        String fileContent = "";
-        // printing the contents of the file
-        for(int i=0; i<file.numberOfColumns(); i++){
-            fileContent += String.format("%-25s",content.getColumns().get(i));
-            if(fileType.equals("csv")){
-                fileContent += ",";
-            }
-            columns[i] = content.getColumns().get(i);
-        }
-
-        fileContent += "\n";
-
-
-        for(int i=0; i<file.numberOfRows(); i++){
-            for(int j =0; j<file.numberOfColumns();j++){
-                fileContent += String.format("%-24s ", content.getValues(columns[j]).get(i));
-                if(fileType.equals("csv")){
                     fileContent += ",";
                 }
             }
